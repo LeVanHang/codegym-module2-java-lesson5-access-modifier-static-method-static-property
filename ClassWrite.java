7946f1de03be6fb8e93fdc175fcafef93dc23018
LeVanHang <vanhang1802@gmail.com>
2022-08-12 07:03:14 +0700

    ex access done

---
+++ ClassWrite.java
@@ -0,0 +1,24 @@
+public class ClassWrite {
+    private String name = "john";
+    private String Classes = "c02";
+
+    public ClassWrite() {
+    }
+
+    public ClassWrite(String name, String classes) {
+        this.name = name;
+        Classes = classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        Classes = classes;
+    }
+
+   public class Test {
+
+   }
+}
